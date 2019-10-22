6a39a5fb47a7585a068874ebdd1925f76b7944d2
noorahmedds <noorahmedds@outlook.com>
2019-10-22 11:23:48 +0500

    Added object size calculation

--- cvUtils.py
+++ cvUtils.py
@@ -6,8 +6,29 @@ import face_recognition
 from imutils import face_utils
 import dlib
 import time
+import sys
 
 Point2D = namedtuple("Point2D", "x y")
+
+def get_size(obj, seen=None):
+    """Recursively finds size of objects"""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, '__dict__'):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_size(i, seen) for i in obj])
+    return size
 
 def find_dominant_color(img, verbose = False):
     """Finds Dominant color from an image. Returns palette and the dominant RGB value for that image"""
