743240ec8555da3db08f55c6aa8c5c915c311b4e
noorahmedds <noorahmedds@outlook.com>
2019-10-30 18:35:17 +0500

    Added face pose estimation

--- cvUtils.py
+++ cvUtils.py
@@ -10,6 +10,22 @@ import sys
 
 Point2D = namedtuple("Point2D", "x y")
 
+
+def clamp(value, between):
+    """
+    Clamp value between a range.
+    """
+    return max(between[0], min(value, between[1]))
+
+
+def doOverlap(boxA, boxB):
+    if (boxA[0] > boxB[2] or boxB[0] > boxA[2]):
+        return False
+    if (boxA[1] > boxB[3] or boxB[1] > boxA[3]):
+        return False
+    return True
+
+
 def get_size(obj, seen=None):
     """Recursively finds size of objects"""
     size = sys.getsizeof(obj)
@@ -30,7 +46,32 @@ def get_size(obj, seen=None):
         size += sum([get_size(i, seen) for i in obj])
     return size
 
-def find_dominant_color(img, verbose = False):
+
+def iou(boxA, boxB):
+    """
+    Calculate iou between two bounding boxes
+    """
+
+    # Determine the coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    # Compute the area of intersection
+    intersection_area = (xB - xA + 1) * (yB - yA + 1)
+
+    # Compute the area of both rectangles
+    boxA_area = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxB_area = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+    # Compute the IOU
+    iou = float(intersection_area) / \
+        float(boxA_area + boxB_area - intersection_area)
+    return iou
+
+
+def find_dominant_color(img, verbose=False):
     """Finds Dominant color from an image. Returns palette and the dominant RGB value for that image"""
 
     # Average
@@ -38,14 +79,15 @@ def find_dominant_color(img, verbose = False):
 
     pixels = np.float32(img.reshape(-1, 3))
     n_colors = 5
-    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 500, .05) #the final param is epsilon. It is the minimum amount the centroid has to move before the update is terminated
+    # the final param is epsilon. It is the minimum amount the centroid has to move before the update is terminated
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 500, .05)
     flags = cv.KMEANS_RANDOM_CENTERS
 
     # Using kmeans to determine n_colors centroids from the complete set of pixel values
     _, labels, palette = cv.kmeans(pixels, n_colors, None, criteria, 10, flags)
     _, counts = np.unique(labels, return_counts=True)
     dominant = np.int0(palette[np.argmax(counts)])
-    
+
     if verbose:
         print("Palette: ", palette)
         print("Labels: ", labels)
@@ -56,10 +98,11 @@ def find_dominant_color(img, verbose = False):
     # display_color(counts, img, palette)
     # Swapping R channel with B
     dominant = dominant[::-1]
-    
+
     return [palette], [dominant]
 
-def imshow2(im_name, image, location = [0, 0], resize = Point2D(x=350, y=600)):
+
+def imshow2(im_name, image, location=[0, 0], resize=Point2D(x=350, y=600)):
     """
     Shows the image in the upper left side (by default) of the screen while using the cv2.imshow functionality
 
@@ -77,12 +120,13 @@ def imshow2(im_name, image, location = [0, 0], resize = Point2D(x=350, y=600)):
     Returns
     -------
     null : void
-    
+
     """
     cv.namedWindow(im_name, cv.WINDOW_NORMAL)
     cv.moveWindow(im_name, location[0], location[1])
     cv.resizeWindow(im_name, resize.x, resize.y)
     cv.imshow(im_name, image)
+
 
 def waitAfterShow(key='q', verbose=False):
     """
@@ -102,18 +146,21 @@ def waitAfterShow(key='q', verbose=False):
     """
     while True:
         wait = cv.waitKey(0)
-        if verbose: print(wait)
+        if verbose:
+            print(wait)
         if wait & 0xFF == ord(key):
             cv.destroyAllWindows()
             return
 
+
 def processFrame(image):
     return imutils.auto_canny(image)
 
-def streamProcessedVideo(videoPath, processFrame, willProcess = True, showFPS = True):
+
+def streamProcessedVideo(videoPath, processFrame, willProcess=True, showFPS=True):
     """
     The function will show a processed stream of a video
-    
+
     Parameters
     ----------
     processFrame: a callable function object
@@ -128,7 +175,6 @@ def streamProcessedVideo(videoPath, processFrame, willProcess = True, showFPS = 
     null : void
     """
     cap = cv.VideoCapture(videoPath)
-
 
     # FPS
     if showFPS:
@@ -145,7 +191,7 @@ def streamProcessedVideo(videoPath, processFrame, willProcess = True, showFPS = 
         else:
             out = gray
 
-        imshow2('frame',out)
+        imshow2('frame', out)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -154,7 +200,8 @@ def streamProcessedVideo(videoPath, processFrame, willProcess = True, showFPS = 
             end = time.time()
             seconds = end-start
             fps = (fps + (1/seconds))/2
-            cv2.putText(out, "{0} fps".format(fps), (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3)
+            cv2.putText(out, "{0} fps".format(fps), (10, 50),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
             print("{0} fps".format(fps))
             start = end
 
@@ -162,10 +209,10 @@ def streamProcessedVideo(videoPath, processFrame, willProcess = True, showFPS = 
     cv.destroyAllWindows()
 
 
-def streamProcessedWebcam(processFrame, willProcess = True, showFPS = True):
+def streamProcessedWebcam(processFrame, willProcess=True, showFPS=True):
     """
     The function will show a processed stream of the webcam capture
-    
+
     Parameters
     ----------
     processFrame: a callable function object
@@ -190,7 +237,7 @@ def streamProcessedWebcam(processFrame, willProcess = True, showFPS = True):
     while(True):
         ret, frame = cap.read()
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        
+
         # Processing occurs here
         if willProcess:
             out = processFrame(gray)
@@ -207,13 +254,110 @@ def streamProcessedWebcam(processFrame, willProcess = True, showFPS = True):
             end = time.time()
             seconds = end-start
             fps = (fps + (1/seconds))/2
-            cv2.putText(out, "{0} fps".format(fps), (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3)
+            cv2.putText(out, "{0} fps".format(fps), (10, 50),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
             print("{0} fps".format(fps))
             start = end
 
     # When everything done, release the capture
     cap.release()
     cv.destroyAllWindows()
+
+def est_pose(im, bbox):
+    """
+    Adapted from learnopencv.com script to include dlibs shape predictors input. Returns image and whether it is a frontal face or not
+    
+    Parameters
+    ----------
+    im: frame from image or video
+    bbox: the shape array converted to a numpy array using imutils shape_to_np
+
+    Returns
+    -------
+    im: with gui
+    frontal: flag whether frontal or not
+
+    """
+    gray = cv2.cvtColor(im, 7)
+    cv2.imshow("crop", im[bbox[1]:bbox[1]+bbox[3], bbox[0]:bbox[0]+bbox[2]])
+    tl = np.array((bbox[0], bbox[1]))
+    shape = predictor(gray, dlib.rectangle(
+        bbox[0], bbox[1], bbox[2]+bbox[0], bbox[3]+bbox[1]))
+    # Shape becomes a 2 dim array
+    shape = shape_to_np(shape)
+
+    im = visualize_facial_landmarks(im, shape)
+
+    size = im.shape
+
+    # shape += tl
+    # 2D image points. If you change the image, you need to change vector
+    image_points = np.array([
+        shape[33],     # Nose tip
+        shape[8],     # Chin
+        shape[36],     # Left eye left corner
+        shape[45],     # Right eye right corne
+        shape[48],     # Left Mouth corner
+        shape[54]      # Right mouth corner
+    ], dtype="double")
+
+    # Adding top_left offset
+    # image_points += tl
+
+    # 3D model points.
+    model_points = np.array([
+        (0.0, 0.0, 0.0),             # Nose tip
+        (0.0, -330.0, -65.0),        # Chin
+        # Left eye left corner
+        (-225.0, 170.0, -135.0),
+        # Right eye right corne
+        (225.0, 170.0, -135.0),
+        # Left Mouth corner
+        (-150.0, -150.0, -125.0),
+        # Right mouth corner
+        (150.0, -150.0, -125.0)
+
+    ])
+
+    # Camera internals
+
+    focal_length = size[1]
+    center = (size[1]/2, size[0]/2)
+    camera_matrix = np.array(
+        [[focal_length, 0, center[0]],
+         [0, focal_length, center[1]],
+         [0, 0, 1]], dtype="double"
+    )
+
+    print("Camera Matrix :\n {0}".format(camera_matrix))
+
+    dist_coeffs = np.zeros((4, 1))  # Assuming no lens distortion
+    (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points,
+                                                                  image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
+
+    print("Rotation Vector:\n {0}".format(rotation_vector))
+    print("Translation Vector:\n {0}".format(translation_vector))
+
+    # Project a 3D point (0, 0, 1000.0) onto the image plane.
+    # We use this to draw a line sticking out of the nose
+
+    (nose_end_point2D, jacobian) = cv2.projectPoints(np.array(
+        [(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+
+    for p in image_points:
+        cv2.circle(im, (int(p[0]), int(p[1])), 3, (0, 0, 255), -1)
+
+    p1 = (int(image_points[0][0]), int(image_points[0][1]))
+    p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+
+    cv2.line(im, p1, p2, (255, 0, 0), 2)
+
+    # cv2.imshow("out", im)
+    frontal = False
+    if abs(rotation_vector[2]) < 0.7:
+        frontal = True
+
+    return im, frontal
 
 
 if __name__ == "__main__":
@@ -236,14 +380,14 @@ if __name__ == "__main__":
     #     rotated = imutils.rotate(gray, angle=angle)
     #     rot.append(cv.cvtColor(rotated, cv.COLOR_GRAY2RGB))
     #     # cv.imshow("Angle=%d" % (angle), rotated)
-    
+
     # # Refer to https://pyformat.info for formatting
     # print("Showing file {0}, {1:.5}".format(1, im_path))
-    
+
     # mont = imutils.build_montages(rot, (200, 200), (5, 1))
     # for m, i in zip(mont, range(0, len(mont))):
     #     cv.imshow("Montage %i" % i, mont[i])
-    
+
     # waitAfterShow()
 
     streamProcessedWebcam(processFrame)
